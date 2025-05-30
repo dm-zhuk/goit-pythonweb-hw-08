81847c590ca9f52e26125ce12ca933eb3de5b2de
Dmytro Zhuk <140021292+dm-zhuk@users.noreply.github.com>
2025-05-31 01:08:33 +0300

    directory clean

--- src/repository/owners.py
+++
@@ -1,9 +0,0 @@
-from api.databases.models import Owner
-
-
-async def create_owner(body, db):
-    owner = Owner(**body.model_dump())
-    db.add(owner)
-    db.commit()
-    db.refresh(owner)
-    return owner
