4b85d81133e23d6f4c2d8f046d2b9f8aec083ed0
Dmytro Zhuk <140021292+dm-zhuk@users.noreply.github.com>
2025-05-31 00:10:17 +0300

    routers, main.py added

--- src/main.py
+++ src/main.py
@@ -1,43 +1,10 @@
-from fastapi import FastAPI, Depends, HTTPException
-from sqlalchemy import text
+from fastapi import FastAPI
+from routers.contacts import router
 
-from api.databases.connect import get_db
-from api.routers import cats, owners
+app = FastAPI(title="Contacts API", description="REST API for managing contacts")
+app.include_router(router)
 
-app = FastAPI()
+if __name__ == "__main__":
+    import uvicorn
 
-
-@app.get("/", name="API root")
-def read_root():
-    return {"message": "Welcome to fast_api_project"}
-
-
-@app.get("/health", name="Service availability")
-def get_health_status(db=Depends(get_db)):
-    try:
-        result = db.execute("SELECT 1+1").fetchone()
-        print(result)
-        if result is None:
-            raise Exception
-        return {"message": "Service is running"}
-    except Exception as e:
-        print(f"Database connection error: {e}")
-        raise HTTPException(status_code=500, detail="Database connection failed")
-    return {"status": "ok", "message": "Service is running"}
-
-
-# general
-# - root
-# - health
-
-# cats
-# _create cat
-# - delete cat
-# - list cat
-# - update cat
-
-# owner
-# _ create owner
-# - delete owner
-# - list owner
-# - update owner
+    uvicorn.run(app, host="0.0.0.0", port=8000)
--- src/routers/cats.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-# localhost:8000/owners/<int:owner_id>
-
-router=APIRouter(prefix="/owners", tags=["owners"])
-
-@router.post("/", name="Create owner")
